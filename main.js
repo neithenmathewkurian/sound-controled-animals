e20f760ce8da6ae52397b1a754bc4f551ab191e9
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2022-02-09 18:54:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier("")
+        ;
+
+
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+
+    }
+    else {
+
+        console.log(results);
+        var sound1 = results[0].label;
+        var confidence = results[0].confidence;
+        document.getElementById("result_label").innerHTML = "i can hear-" + sound1;
+        document.getElementById("result_confidence").innerHTML = "accuracy-" + confidence.toFixed(3) * 100 + "%";
+        img1 = document.getElementById("cat.jpg");
+
+        if (sound1 == "barking") {
+            img1.src = "doggie.jpg";
+
+        }
+        else if (sound1 == "meow") {
+            img1.src = "cat.jpg";
+
+        }
+
+
+    }
+}
